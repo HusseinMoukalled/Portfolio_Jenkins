3eaaea07e2a5c42319d3b507d4aeda37a9c7a34f
HusseinMoukalled <ham69@mail.aub.edu>
2025-06-22 17:35:36 +0300

    V7

--- build-and-deploy.bat
+++ build-and-deploy.bat
@@ -1,20 +1,19 @@
 @echo off
-REM Use Minikube's Docker daemon
-FOR /f "tokens=*" %%i IN ('minikube docker-env') DO @%%i
-
-REM Generate unique image tag using random + time
+REM Build fresh image in HOST Docker
 SET TAG=%RANDOM%-%TIME:~6,2%%TIME:~3,2%%TIME:~0,2%
 
-REM Build fresh tagged image inside Minikube
 docker build -t portfolio:%TAG% .
+
+REM Push the image into Minikube's internal Docker
+minikube image load portfolio:%TAG%
 
 REM Force kubectl to use your kubeconfig
 SET KUBECONFIG=C:\Users\Hussein\.kube\config
 
-REM Apply deployment YAML in case (structure remains)
+REM Apply deployment YAML (structure stays same)
 kubectl apply -f k8s\deployment.yaml
 
-REM Overwrite Deployment image to this unique tag (forces new pod)
+REM Update Deployment to use the new tag (forces new pod)
 kubectl set image deployment/portfolio-deployment portfolio=portfolio:%TAG%
 
 REM Apply service YAML
