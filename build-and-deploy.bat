20bfdea360c509774a74fabea52f510c64c8619e
HusseinMoukalled <ham69@mail.aub.edu>
2025-06-22 17:54:05 +0300

    V8

--- build-and-deploy.bat
+++ build-and-deploy.bat
@@ -1,28 +1,24 @@
 @echo off
-REM Ensure Jenkins uses YOUR user’s Minikube + Kube config
-SET MINIKUBE_HOME=C:\Users\Hussein\.minikube
-SET USERPROFILE=C:\Users\Hussein
-SET KUBECONFIG=C:\Users\Hussein\.kube\config
-
 REM Generate unique tag
 SET TAG=%RANDOM%-%TIME:~6,2%%TIME:~3,2%%TIME:~0,2%
 
-REM Build in host Docker
+REM Build locally
 docker build -t portfolio:%TAG% .
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
-REM Load into Minikube, force profile, fail if error
-minikube image load portfolio:%TAG% --profile=minikube
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+REM Tag for local registry
+docker tag portfolio:%TAG% localhost:5000/portfolio:%TAG%
+
+REM Push to local registry
+docker push localhost:5000/portfolio:%TAG%
+
+REM Use kubeconfig
+SET KUBECONFIG=C:\Users\Hussein\.kube\config
 
 REM Apply deployment
 kubectl apply -f k8s\deployment.yaml
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
-REM Set image to unique tag
-kubectl set image deployment/portfolio-deployment portfolio=portfolio:%TAG%
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+REM Force deployment to use new image from local registry
+kubectl set image deployment/portfolio-deployment portfolio=localhost:5000/portfolio:%TAG%
 
 REM Apply service
 kubectl apply -f k8s\service.yaml
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
